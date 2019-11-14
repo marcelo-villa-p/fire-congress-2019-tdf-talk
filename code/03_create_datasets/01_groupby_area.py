6ff44f43469f5ceff6a1d66cea462595dca26e9b
Marcelo Villa <marcelo.villa.p@gmail.com>
2019-11-14 09:49:23 -0500

    fixed multiple small bugs and added rescaling to the evi and evi_prev column values

--- code/03_create_datasets/01_groupby_area.py
+++ code/03_create_datasets/01_groupby_area.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 
 from code.functions import create_data_array, get_nodata_value
+from code.variables import evi_scaling_factor
 
 
 def get_data_statistic(data, nd, start=0, end=None, statistic='mean'):
@@ -35,7 +36,7 @@ def get_data_statistic(data, nd, start=0, end=None, statistic='mean'):
         raise NotImplementedError()
 
 
-if __name__ == '__main':
+if __name__ == '__main__':
     # change directory
     os.chdir('../../data/tif')
 
@@ -44,17 +45,17 @@ if __name__ == '__main':
     df = pd.DataFrame(columns=columns)
 
     # define date range
-    date_range_off = pd.date_range('2002', '2017', freq='MS', closed='left')
+    date_range_off = pd.date_range('2001-10-01', '2016-12-31', freq='MS')
     date_range = date_range_off[3:]
 
     # define products and their properties
     products = [
         {'path': 'MODIS/MOD14A2/prepared', 'col': 'fire_pixels', 'stat': 'sum',
          'compute_prev': False, 'date_range': date_range},
-        {'path': 'MODIS/MOD13A3/prepared', 'col': 'ppt', 'stat': 'mean',
+        {'path': 'MODIS/MOD13A3/prepared', 'col': 'evi', 'stat': 'mean',
          'compute_prev': True, 'date_range': date_range_off},
         {'path': 'TRMM/3B43/prepared', 'col': 'ppt', 'stat': 'mean',
-         'compute_prev': True, 'date_range': date_range},
+         'compute_prev': True, 'date_range': date_range_off},
     ]
 
     for prod in products:
@@ -77,5 +78,9 @@ if __name__ == '__main':
     df['fire_pixels'] = df['fire_pixels'].astype('int')
     df[df.columns[1:]] = df[df.columns[1:]].astype('float')
 
+    # rescale EVI values using a defined scaling factor
+    df['evi'] = df['evi'] * evi_scaling_factor
+    df['evi_prev'] = df['evi_prev'] * evi_scaling_factor
+
     # save DataFrame to csv
-    df.to_csv('../csv/groupby_area.csv', index=False)
+    df.to_csv('../csv/groupby_area.csv', index=True, index_label='date')
