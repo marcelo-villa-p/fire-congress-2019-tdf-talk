dfc107cdd0904421b5dd4252309328c8782109dd
Marcelo Villa <marcelo.villa.p@gmail.com>
2019-11-14 01:39:03 -0500

    added file to store variables used across multiple files

---
+++ code/variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# =============================================================================
+# Date:     February, 2019
+# Author:   Marcelo Villa P.
+# Purpose:  Contains variables shared across multiple scripts in the project.
+# =============================================================================
+# general
+bbox = (-78.9909352282, -4.29818694419, -66.8763258531, 12.4373031682)
+landcovers = {1: 'Forest', 2: 'Savanna', 3: 'Grassland', 4: 'Cropland'}
+
+# plots
+edge_color = '#102027'
+face_color = '#102027'
