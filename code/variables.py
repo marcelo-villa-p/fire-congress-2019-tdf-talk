d1fd9bbf872bbded3ac30436bdc296d53bb6e56b
Marcelo Villa <marcelo.villa.p@gmail.com>
2019-11-16 09:30:29 -0500

    added evi_scaling_factor and color variables used in the plots

--- code/variables.py
+++ code/variables.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # =============================================================================
-# Date:     February, 2019
+# Date:     November, 2019
 # Author:   Marcelo Villa P.
 # Purpose:  Contains variables shared across multiple scripts in the project.
 # =============================================================================
 # general
 bbox = (-78.9909352282, -4.29818694419, -66.8763258531, 12.4373031682)
 landcovers = {1: 'Forest', 2: 'Savanna', 3: 'Grassland', 4: 'Cropland'}
+evi_scaling_factor = 0.0001
 
-# plots
-edge_color = '#102027'
-face_color = '#102027'
+# colors
+# edge_color = '#102027'
+edge_color = '#23373B'
+# face_color = '#FFFFFF'
+face_color = '#FAFAFA'
+hue_one = '#23373B'
+hue_two = '#EB811B'
